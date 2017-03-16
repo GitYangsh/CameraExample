3268dba26c02fc6c7b23fbcc27c206d1e3163873
yangsh <764829761@qq.com>
2017-03-16 18:45:38 +0800

    first commit

---
+++ app/src/main/java/com/example/ysh/camera/Camera2Activity.java
@@ -0,0 +1,10 @@
+package com.example.ysh.camera;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ysh on 2017/3/16.
+ */
+
+public class Camera2Activity extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/example/ysh/camera/CameraActivity.java
@@ -0,0 +1,18 @@
+package com.example.ysh.camera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ysh on 2017/3/16.
+ */
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
